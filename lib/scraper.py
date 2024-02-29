7863fd00e92260dfdac361239960d1a811ce9832
mmwandera <mmwandera74@gmail.com>
2024-02-29 15:34:52 +0300

    completed code along

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,11 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://moringaschool.com/courses/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+# print (doc.prettify())
+
+print(doc.select('.page-title.leading-tight')[0].contents)
+print(doc.select('.page-title.leading-tight')[0].name)
+print(doc.select('.page-title.leading-tight')[0].attrs)
